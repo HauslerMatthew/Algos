84b1db7e8c768d4e8cf6959b832f8abbc0fcfe93
Matthew Hausler <74732297+HauslerMatthew@users.noreply.github.com>
2023-06-16 16:28:20 -0500

    Add files via upload

--- robots.js
+++ robots.js
@@ -1,21 +1,22 @@
-// Two robots are dropped from an airplane and land on a single line (with discrete positions) using a parachute which is left at the landing point. The robots are both facing north, they are an unknown distance apart, and one has landed directly east of the other.
+// Two robots are dropped from an airplane and land on a single line (with discrete positions) using a parachute which is left at the landing point. The robots are an unknown distance apart, and one has landed directly east of the other.
 
 // The robots are now to be programmed such that they meet each other. They can be instructed to move left or right to a neighboring position and to check whether a parachute is present at the current location. If the other robot is met both robots stop there and live happily ever after.
 
 // The parachute check might conditionally execute any number of instructions and any block of instructions may be repeated unconditionally. Write down a program that both robots can follow simultaneously and which guarantees that they meet.
 
 // --------chute count-------------------------1-----------1--------------------------------------------//
-// --------------------------------------------robot 1-----robot 2---------------------------------------------//
-// -----------------------------------------------para-----para-------------------------------------------//
+// ------------------------------------------robot 1-----robot 2----------------------------------------//
+// --------------------------------------------para---------para----------------------------------------//
 
-// bots will move left once , right twice, left three times (repeat this pattern indefintely)
+// solution 1:
 
-// after each unit of movement in a repeated direction, run a chute check.  when chute check = true, increment chute counter to one.
+// bots move left (repeat this pattern indefintely)
 
-// once they start moving the opposite direction, reset chute count to 0
+// after each unit of movement, stop and run a chute check - continue this for as long as chute check = false.  
 
-// ONE bot eventually finds the others chute, meaning its chute count will = 2. 
+// When chute check = true, this bot continues left (no checks, no stoppages until it catches other bot).
 
-// if chute count = 2; do nothing in perpetuity
 
-// eventually, the other bot starts coming back to its original chute, at which point when it hits the other bot. 
+
+
+
