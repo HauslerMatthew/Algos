113693a9cf6ffa85365fe2c6c5b47127eb055ed6
Matthew Hausler <74732297+HauslerMatthew@users.noreply.github.com>
2023-06-17 18:41:38 -0500

    Add files via upload
    practice building functions that mimic string.split, .reverse (convert string to array of words)

---
+++ stringToWordArray.js
@@ -0,0 +1,30 @@
+var myString = "this is a test."
+
+const stringToArray = str => str.split(" ");
+
+console.log(stringToArray(myString));
+
+const reverseStringToArray = str => str.split(" ").reverse();
+
+console.log(reverseStringToArray(myString));
+
+
+// recreate the functions above using long methods (no built-ins)
+
+function stringToWordArray(stringInput){
+    let result = [];
+    let word = '';
+    for (var i=0;i<stringInput.length;i++){
+        if (stringInput[i] != " "){
+            word += stringInput[i]; //add each "letter" to word until you encounter a space character
+        }
+        else if (stringInput[i] == " "){
+            result.push(word); //once you have hit a space, add the word to the result array
+            word = "" //then reset the word value to nothing and start adding again
+        }
+    }
+    result.push(word); //need this to push the last word (before sentence has ended) to array.
+    return result;
+}
+console.log(stringToWordArray(""));
+console.log(stringToWordArray("Hello there, how are you?"));
