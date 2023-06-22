1b2f7da0e4fd9f55dcdf6f74952222c642b6d3d3
Matthew Hausler <74732297+HauslerMatthew@users.noreply.github.com>
2023-06-22 16:20:53 -0500

    Add files via upload
    Working on the reverse portion without a prebuilt.

--- stringToWordArray.js
+++ stringToWordArray.js
@@ -28,3 +28,22 @@ function stringToWordArray(stringInput){
 }
 console.log(stringToWordArray(""));
 console.log(stringToWordArray("Hello there, how are you?"));
+
+function stringToWordArrayReverse(stringInput){
+    let result = [];
+    let word = '';
+    for (var i=0;i<stringInput.length;i++){
+        if (stringInput[i] != " "){
+            word += stringInput[i]; //add each "letter" to word until you encounter a space character
+        }
+        else if (stringInput[i] == " "){
+            result.push(word); //once you have hit a space, add the word to the result array
+            word = "" //then reset the word value to nothing and start adding again
+        }
+    }
+    result.push(word); //need this to push the last word (before sentence has ended) to array.
+    result.reverse();
+    return result;
+}
+
+console.log(stringToWordArrayReverse("Hello there, how are you?"));
