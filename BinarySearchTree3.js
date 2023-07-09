92743c521e7ee0371032a8e342ffdaf5df2c9eab
Matthew Hausler <74732297+HauslerMatthew@users.noreply.github.com>
2023-07-09 12:35:55 -0500

    Add files via upload

--- BinarySearchTree3.js
+++ BinarySearchTree3.js
@@ -45,6 +45,7 @@ class BST{
     getRootNode() { 
         return this.root; 
     }
+    //new function, print the full tree (will have to get creative with the console)
     printTree(node){
         console.log("Current node=",node.value)
         if(node.left != null){
@@ -56,14 +57,17 @@ class BST{
             this.printTree(node.right);
         }
     }
+    //find the height of the tree
     height(root){ 
         if(root == null){
             return 0;
         }
         var leftHeight = this.height(root.left);
         var rightHeight = this.height(root.right);
+        //will need to compare each side from the root to find the taller side, then increment 
         return Math.max(leftHeight, rightHeight) + 1;
     }
+    //check if BST is balanced (same length each side from root)
     isBalanced(root){
         if(root == null){
             return true;
