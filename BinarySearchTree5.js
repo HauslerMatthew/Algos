9f64001d2469969bd5b14bda9c1313e568d81f57
Matthew Hausler <74732297+HauslerMatthew@users.noreply.github.com>
2023-07-18 19:12:54 -0500

    Add files via upload
    added notes that explains code blocks

--- BinarySearchTree5.js
+++ BinarySearchTree5.js
@@ -70,25 +70,37 @@ class BST {
             this.printTree(node.right);
         }
     }
-    sortedArrToBST(arr){
-        if(arr.length == 0){
+
+    //using existing functions and constructors, make a function that accepts a sorted array, then converts it into a BST.
+    sortedArrToBST(arr) {
+        //cornercase 1, array is empty
+        if (arr.length == 0) {
             console.log("Get a larger array");
             return null;
         }
-        var midPoint = arr[Math.floor(arr.length/2)];
+        //BST's need a balance between values (root should be middle value of array) otherwise you end up with a linked list.
+        var midPoint = arr[Math.floor(arr.length / 2)];
+        //begin creating your BST after you find middle balance to split out values
         this.insert(midPoint);
-        var rightSide = arr.slice([Math.floor(arr.length/2)+1]);
-        var leftSide = arr.slice(0, Math.floor(arr.length/2));
-        if (rightSide.length > 1){
+        //then what will you do with remaining values to the left and right of it?
+        //we have a previous block of code from array Binary sorts (we decrease length of the array until we find a target)
+        //this creates 2 new arrays from the left and right
+        var rightSide = arr.slice([Math.floor(arr.length / 2) + 1]);
+        var leftSide = arr.slice(0, Math.floor(arr.length / 2));
+        //since our logic is similar with these new arrays (we cannot just insert the arrays or else we end up with straight lines rather than left and right branches)
+        //so if our right side has multiple values, we run it through the function again to balance it out at a central point
+        if (rightSide.length > 1) {
             this.sortedArrToBST(rightSide);
         }
-        if (rightSide.length == 1){
+        //once we have one value to the right, we simply insert that value into our BST (the insert function does the BST logic on where it goes in the tree)
+        if (rightSide.length == 1) {
             this.insert(rightSide[0]);
         }
-        if (leftSide.length > 1){
+        //repeat for left side of array
+        if (leftSide.length > 1) {
             this.sortedArrToBST(leftSide);
         }
-        if (leftSide.length == 1){
+        if (leftSide.length == 1) {
             this.insert(leftSide[0]);
         }
     }
@@ -102,7 +114,9 @@ var root = myBST.getRootNode();
 myBST.printTree(root);
 
 
+//condensed version
 
+//sortedArrToBST(arr){
 // if(arr.length < 1){
 //     return null;
 // }
@@ -113,3 +127,4 @@ myBST.printTree(root);
 // root.right = this.sortedArrToBST(arr.slice(mid+1, arr.length));
 
 // return root;
+//}
